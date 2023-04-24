f8cf92f839335b5542a3f1a10662f26fcacfb3df
tristanretali <tristan.retali@gmail.com>
2023-04-24 15:37:41 +0200

    refactor: :recycle: Change the column name of the final dataset

--- dataset.py
+++ dataset.py
@@ -3,59 +3,84 @@ import csv
 import os
 
 # Path for each dataset
-DISEASE_SYMPTOMS_1 = "./datasets/original_datasets/disease_symptoms.csv"
-DISEASE_SYMPTOMS_2 = "./datasets/original_datasets/disease_and_symptoms.csv"
-DISEASE_SYMPTOMS_3= "./datasets/original_datasets/symptoms_research_paper.csv"
+DISEASE_SYMPTOMS_1 = "./datasets/original_datasets/diseases_symptoms_1.csv"
+DISEASE_SYMPTOMS_2 = "./datasets/original_datasets/diseases_symptoms_2.csv"
+DISEASE_SYMPTOMS_3= "./datasets/original_datasets/diseases_symptoms_3.csv"
 FORMATTING_DATASET = "./datasets/formatting_dataset.csv"
 DATASET = "./datasets/dataset.csv"
 
-def delete_files():
-    os.remove(FORMATTING_DATASET)
-   # os.remove(DATASET)
-
 def create_dataset():
     """
-    Create the final dataset
+    Create the final dataset which include 
+    symptoms for each diseases
     """
-    disease_symptoms_formatting()
-    disease_and_symptoms_formatting()
+    disease_symptoms_1_formatting()
+    disease_symptoms_2_formatting()
+    disease_symptoms_3_formatting()
 
-def disease_symptoms_formatting():
+def disease_symptoms_1_formatting():
+    """
+    Fill the dataset with the disease
+    and symptoms data from the first 
+    dataset
+    """
+    data = {}
     df = pd.read_csv(DISEASE_SYMPTOMS_1)
     df.drop_duplicates(subset="Disease", keep="first", inplace=True) 
-    with open(FORMATTING_DATASET, 'x') as csvfile:
+    with open(DATASET, 'x') as csvfile:
         filewriter = csv.writer(csvfile, delimiter=',', lineterminator='\n')
-        filewriter.writerow(df.columns)
+        filewriter.writerow(["disease", "symptoms"])
         for index, row in df.iterrows():
-            filewriter.writerow(row)
+            for symptom in row.items():
+                if is_string_type(symptom):
+                    if not row["Disease"] in data:                        
+                        data[row["Disease"]] = ""
+                    else:
+                        data[row["Disease"]] += f"{symptom[1]} "
+        filewriter.writerows(data.items())
 
-def disease_and_symptoms_formatting():
-    usecols = ["name", "symptoms"]
+def disease_symptoms_2_formatting():
+    """
+    Fill the dataset with the disease
+    and symptoms data from the second 
+    dataset
+    """
+    usecols = ["Disease", "symptoms"]
+    data = {}
     df = pd.read_csv(DISEASE_SYMPTOMS_2, usecols=usecols)
-    with open(FORMATTING_DATASET, 'a') as csvfile:
+    with open(DATASET, 'a') as csvfile:
         filewriter = csv.writer(csvfile, delimiter=',', lineterminator='\n')       
         for index, row in df.iterrows():
             symptoms = parse_symptoms(row)
-            filewriter.writerow(symptoms)
+            data[row["Disease"]] = ""
+            for symptom in symptoms:
+                data[row["Disease"]] += f"{symptom} "
+        filewriter.writerows(data.items())
 
-def symptoms_research_paper_formatting():
+def disease_symptoms_3_formatting():
+    """
+    Fill the dataset with the disease
+    and symptoms data from the third 
+    dataset
+    """
     usecols=["Symptom", "Disease"]
-    all_diseases = {}
+    data = {}
     df = pd.read_csv(DISEASE_SYMPTOMS_3, usecols=usecols, delimiter='	')
-    with open(FORMATTING_DATASET, 'a') as csvfile:
+    with open(DATASET, 'a') as csvfile:
         filewriter = csv.writer(csvfile, delimiter=',', lineterminator='\n')
         for index, row in df.iterrows():        
-            fill_dict(all_diseases, row)
-        
-        for value in all_diseases.values():
-            filewriter.writerow(value)
+            if not row["Disease"] in data:
+                data[row["Disease"]] = f"{row['Symptom']} "
+            else:
+                data[row["Disease"]] += f"{row['Symptom']} "
+        filewriter.writerows(data.items())
 
 def parse_symptoms(row: any) ->list :
     """
     Parse the current row
 
     Args:
-        row (any): the current row of the dataset
+        row (Series): the current row of the dataset
 
     Returns:
         list: return all symptoms of the current row
@@ -63,7 +88,7 @@ def parse_symptoms(row: any) ->list :
     symptoms = row["symptoms"].split("{")
     for i in range (len(symptoms)):
         symptoms[i] = symptoms[i][symptoms[i].find(':')+1:symptoms[i].find('}')]
-    symptoms[0] = row["name"]
+    del symptoms[0]
     delete_symptoms_id(symptoms)
     return symptoms
 
@@ -93,16 +118,21 @@ def is_a_valid_symptom(symptom: str) ->bool:
             return False
     return True
 
-def fill_dict(all_diseases: dict, row: any):
-    if not row["Disease"] in all_diseases :
-        all_diseases[row["Disease"]] = [row["Disease"], row["Symptom"]]
-    else:
-        all_diseases.get(row["Disease"]).append(row["Symptom"]) 
+def is_string_type(symptom) ->bool:
+    """
+    Check if the symptom is a string
+    
+    Args:
+        symptom : the current symptom
+
+    Returns:
+        bool: return true iff the symptom is a string
+    """
+    return not isinstance(symptom[1], float)
 
 def main():
-    delete_files()
+    os.remove(DATASET)
     create_dataset()
-    symptoms_research_paper_formatting()
 
 if __name__ == "__main__":
     main()
