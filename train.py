c9552145c68c61c0fbd7a61d476e18c4d988b2fd
tristanretali <tristan.retali@gmail.com>
2023-04-26 10:58:02 +0200

    feat: :sparkles: Added Tokenizer of the symptoms

--- train.py
+++ train.py
@@ -2,10 +2,12 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
+from keras.preprocessing.text import Tokenizer
+from tensorflow.keras.preprocessing.sequence import pad_sequences
 
 DATASET = "./datasets/dataset.csv"
+SEQUENCES_PADDING = 180
 
-df_train = pd.read_csv(DATASET)
 
 def clean_symptoms_data(symptoms: str) -> str:
     
@@ -19,7 +21,6 @@ def clean_symptoms_data(symptoms: str) -> str:
     for symptom in my_symptoms[:]:
         if len(symptom) <= 3:
             my_symptoms.remove(symptom)
-
     symptoms = list_to_string(my_symptoms)
 
     #Remove pre and post Blank Spaces
@@ -33,16 +34,40 @@ def list_to_string(my_symptoms: list) ->str:
         symptoms += f"{symptom} "
     return symptoms
 
-print("Before data treatment : " + df_train["symptoms"][71] + "\n")
+def show_word_distribution(df_train):
+    plt.figure(figsize=(10,8))
+    sns.histplot(data=df_train, x='nb_word')
+    plt.title('Word Count of Symptoms in Train Data after data treatment')
+    plt.xlabel('Word Count')
+    plt.ylabel('Symptom Count')
+    plt.show()
 
-df_train["symptoms"] = df_train["symptoms"].apply(clean_symptoms_data)
+def percentage_word_count(df_train, nb_word: int):
+    train_symptoms = (sum(df_train["nb_word"] < nb_word)/df_train.shape[0])*100
+    print(f"This is the percentage of symptoms information for a disease who have less than {train_symptoms}%")
 
-print("After data treatment : " + df_train["symptoms"][71])
+def x_train_treatment(X_train):
+    tokenizer = Tokenizer()
+    tokenizer.fit_on_texts(X_train)
+    X_train_sequences = tokenizer.texts_to_sequences(X_train)
+    X_train_sequences = pad_sequences(X_train_sequences, padding="post", maxlen=SEQUENCES_PADDING)
+    #print(X_train[0])
+    #print(X_train_sequences[0])
+    #print(len(X_train_sequences[0]))
+    #print(len(X_train_sequences[1]))
+    return X_train_sequences
 
-df_train['word_count'] = df_train['symptoms'].apply(lambda x: len(str(x).split()))
-plt.figure(figsize=(10,8))
-sns.histplot(data=df_train, x='word_count')
-plt.title('Word Count of Symptoms in Train Data after data treatment')
-plt.xlabel('Word Count')
-plt.ylabel('Symptom Count')
-plt.show()
+def main():
+    df_train = pd.read_csv(DATASET)
+    #print("Before data treatment : " + df_train["symptoms"][71] + "\n")
+    df_train["nb_word"] = df_train['symptoms'].apply(lambda x: len(x.split()))
+    df_train["symptoms"] = df_train["symptoms"].apply(clean_symptoms_data)
+    #print("After data treatment : " + df_train["symptoms"][71])
+    #show_word_distribution(df_train)
+    #percentage_word_count(df_train, 180)
+    X_train = df_train["symptoms"]
+    Y_train = df_train["disease"]
+    x_train_sequences = x_train_treatment(X_train)
+
+if __name__ == "__main__":
+    main()
