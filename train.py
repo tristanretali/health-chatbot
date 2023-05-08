db2f5ecbcfb899abd57e6db076b9d25f44b378a2
tristanretali <tristan.retali@gmail.com>
2023-05-08 16:54:49 +0200

    fix: Change the model

--- train.py
+++ train.py
@@ -2,10 +2,12 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
+from tensorflow import keras
 from keras.preprocessing.text import Tokenizer
 from tensorflow.keras.preprocessing.sequence import pad_sequences
+from tensorflow.keras.callbacks import EarlyStopping
 from keras.models import Sequential
-from keras.layers import Dense, Activation, Embedding, Flatten, GlobalMaxPool1D, Dropout, Conv1D, LSTM
+from keras.layers import Dense, Activation, Embedding, Flatten, GlobalMaxPool1D, Dropout, Conv1D, LSTM, SpatialDropout1D, GlobalAveragePooling1D
 from azure.ai.ml import MLClient
 from azure.identity import DefaultAzureCredential
 
@@ -86,15 +88,19 @@ def x_treatment(x_value):
 def create_model(x_train, y_train, x_test, y_test):
     model = Sequential()
     model.add(Embedding(input_dim=VOCAB_SIZE, input_length=SEQUENCES_PADDING, output_dim=OUTPUT_DIM))
-    model.add(Conv1D(filters=128, kernel_size=3, activation='relu'))
-    model.add(GlobalMaxPool1D())
-    model.add(Dense(256, activation="relu"))
-    model.add(Dense(4869, activation="softmax"))
+    '''model.add(SpatialDropout1D(0.2))
+    model.add(LSTM(100, dropout=0.2, recurrent_dropout=0.2))
+    model.add(Dense(10, activation='softmax'))
+    model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])'''
+    model.add(Conv1D(128, 5, activation='relu'))
+    model.add(GlobalAveragePooling1D())
+    model.add(Dense(64, activation='relu'))
+    model.add(Dense(10, activation='softmax'))
+    model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
     model.summary()
-    model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
     #output_array = model.predict(x_value)
     #print(output_array)
-    model.fit(x_train, y_train, epochs=EPOCH, batch_size=BATCH_SIZE, validation_split=0.1, validation_data=(x_test, y_test))
+    model.fit(x_train, y_train, epochs=EPOCH, batch_size=BATCH_SIZE, validation_split=0.1)
     model.save("model")
     score = model.evaluate(x_test, y_test)
     print("Test loss:", score[0])
@@ -102,7 +108,7 @@ def create_model(x_train, y_train, x_test, y_test):
 
 def main():
     df_train = pd.read_csv(DATASET)
-    df_test = pd.read_csv(TEST_DATASET)
+    '''df_test = pd.read_csv(TEST_DATASET)
     #print("Before data treatment : " + df_train["symptoms"][71] + "\n")
     df_train["symptoms"] = df_train["symptoms"].apply(clean_symptoms_data)
     df_test["symptoms"] = df_test["symptoms"].apply(clean_symptoms_data)
@@ -111,14 +117,27 @@ def main():
     #show_word_distribution(df_train)
     #percentage_word_count(df_train, 180)
     #Data to train the model
-    x_train_sequences = x_treatment(df_train["symptoms"])
-    y_train = pd.get_dummies(df_train["disease"], dtype=int).values
+    x_train_sequences = x_treatment(df_train["symptoms"].iloc[:10])
+    #print(df_train["symptoms"].iloc[:10])
+    #print(x_train_sequences)
+    y_train = pd.get_dummies(df_train["disease"].iloc[:10], dtype=int).values
     #print(y_train)
     #Data to test the model
-    x_test_sequences = x_treatment(df_test["symptoms"])
-    y_test = pd.get_dummies(df_test["disease"]).values
+    x_test_sequences = x_treatment(df_test["symptoms"].iloc[:10])
+    y_test = pd.get_dummies(df_test["disease"].iloc[:10], dtype=int).values
+    #print(x_test_sequences)
+    #print(y_test)
     #print(x_train_sequences.shape, y_train.shape)
-    create_model(x_train_sequences, y_train, x_test_sequences, y_test)
+    create_model(x_train_sequences, y_train, x_test_sequences, y_test)'''
+    model = keras.models.load_model('model')
+    tokenizer = Tokenizer()
+    test = ["fatigue weight loss restlessness lethargy irregular sugar level blurred and distorted vision obesity excessive hunger increased appetite polyuria"]
+    seq = tokenizer.texts_to_sequences(test)
+    padded = pad_sequences(seq, maxlen=SEQUENCES_PADDING)
+    labels = df_train["disease"].iloc[:10].values.flatten().tolist()
+    #print(labels)
+    pred = model.predict(padded)
+    print(pred, labels[np.argmax(pred)])
 
 if __name__ == "__main__":
     main()
